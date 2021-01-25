1798fc411cc1df1a9c571b47bfec536c0a668027
gabrielamenezes <gabimenezess10@gmail.com>
2021-01-25 17:57:28 -0300

    index.php

---
+++ controler_project/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Projeto Controle Remoto</h1>
+    <?php
+
+
+    ?>
+</body>
+</html>
