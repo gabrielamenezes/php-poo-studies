9ca368dc5abcb95f880d2e1cd2a3e4491ba5acc2
gabrielamenezes <gabimenezess10@gmail.com>
2021-01-26 19:00:24 -0300

    esqueleto dos métodos da classe livro

--- library_project/Livro.php
+++ library_project/Livro.php
@@ -8,6 +8,63 @@
         private $pagAtual;
         private $aberto;
         private $leitor;
+
+        //Construtor 
+
+        public function __construct($title, $aut, $totp, $pag, $open, $leit)
+        {
+            
+        }
+        //Métodos Públicos
+        public function detalhes() {
+            echo "Título: " . $this->getTitulo();
+            echo "Autor(a): " . $this->getAutor();
+            echo "Páginas: ". $this->getTotPaginas();
+        }
+
+        //Métodos Especiais
+        public function getTitulo() {
+
+        }
+        public function setTitulo() {
+
+        }
+        
+
+        public function getAutor() {
+
+        }
+        public function setAutor() {
+
+        }
+
+        public function getTotPaginas() {
+
+        }
+        public function setTotPaginas() {
+
+        }
+
+        public function getPagAtual() {
+
+        }
+        public function setPagAtual() {
+
+        }
+
+        public function getAberto() {
+
+        }
+        public function setAberto() {
+
+        }
+
+        public function getLeitor() {
+
+        }
+        public function setLeitor() {
+
+        }
     }
 
 ?>
