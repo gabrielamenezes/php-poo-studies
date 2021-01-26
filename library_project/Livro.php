78aab287333a01552fe5bc053e30e237bd86c5b0
gabrielamenezes <gabimenezess10@gmail.com>
2021-01-26 18:24:41 -0300

    add atributos da classe Livro e add relação de agregação entre Pessoa

--- library_project/Livro.php
+++ library_project/Livro.php
@@ -1,4 +1,13 @@
 <?php
-
+    require_once("Pessoa.php");
+    class Livro {
+        //Atributos
+        private $titulo;
+        private $autor;
+        private $totPaginas;
+        private $pagAtual;
+        private $aberto;
+        private $leitor;
+    }
 
 ?>
