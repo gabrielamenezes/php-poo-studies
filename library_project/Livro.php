144a0d8d91bcd4e31d9ef3dccbc44d94b3e84783
gabrielamenezes <gabimenezess10@gmail.com>
2021-01-27 09:57:39 -0300

    Implementação de interface e add métodos getters e setters

--- library_project/Livro.php
+++ library_project/Livro.php
@@ -1,6 +1,7 @@
 <?php
     require_once("Pessoa.php");
-    class Livro {
+    require_once("Publicacao.php");
+    class Livro implements Publicacao {
         //Atributos
         private $titulo;
         private $autor;
@@ -11,7 +12,7 @@
 
         //Construtor 
 
-        public function __construct($title, $aut, $totp, $pag, $open)
+        public function __construct($title, $aut, $totp, $pag)
         {
             $this->setTitulo($title);
             $this->setAutor($aut);
@@ -29,46 +30,65 @@
 
         //Métodos Especiais
         public function getTitulo() {
-
+            return $this->titulo;
         }
-        public function setTitulo() {
-
+        public function setTitulo($titulo) {
+            $this->titulo = $titulo;
         }
         
 
         public function getAutor() {
-
+            return $this->autor;
         }
-        public function setAutor() {
-
+        public function setAutor($autor) {
+            $this->autor = $autor;
         }
 
         public function getTotPaginas() {
-
+            return $this->totPaginas;
         }
-        public function setTotPaginas() {
-
+        public function setTotPaginas($totPaginas) {
+            $this->totPaginas = $totPaginas;
         }
 
         public function getPagAtual() {
-
+            return $this->pagAtual;
         }
-        public function setPagAtual() {
-
+        public function setPagAtual($pagAtual) {
+            $this->pagAtual = $pagAtual;
         }
 
         public function getAberto() {
-
+            return $this->aberto;
         }
         public function setAberto() {
 
         }
 
         public function getLeitor() {
-
+            return $this->getLeitor();
         }
-        public function setLeitor() {
+        public function setLeitor($leitor) {
+            $this->leitor = $leitor;
+        }
 
+        //Métodos da interface
+
+        public function abrir() {
+            $this->setAberto(true);
+        }
+        public function fechar() {
+            $this->setAberto(false);
+        }
+        public function folhear() {
+            echo "<p>Página Atual: " . $this->getPagAtual() . "</p>";
+            for($c = $this->pagAtual; $c <= $this->totPaginas; $c+=10) {
+                echo "<p>Página: " . $this->setPagAtual($this->getPagAtual() + 10). "</p>";
+            }
+        }
+        public function voltarPag()
+        {
+            
         }
     }
 
