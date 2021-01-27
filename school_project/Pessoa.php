8bcec7aec576458486abc32d5d731d1814b87cdc
gabrielamenezes <gabimenezess10@gmail.com>
2021-01-27 18:42:53 -0300

    add de métodos publicos e especiais (getters e setters)

--- school_project/Pessoa.php
+++ school_project/Pessoa.php
@@ -5,6 +5,31 @@
         private $nome;
         private $idade;
         private $sexo;
+
+        //Métodos Públicos
+        public function fazerAniv() {
+            $this->setIdade($this->getIdade() + 1);
+        }
+
+        //Métodos Especiais
+        public function getNome() {
+            return $this->nome;
+        }
+        public function setNome($nome) {
+            $this->nome = $nome;
+        }
+        public function getIdade() {
+            return $this->idade;
+        }
+        public function setIdade($idade) {
+            $this->idade = $idade;
+        }
+        public function getSexo() {
+            return $this->sexo;
+        }
+        public function setSexo($sexo) {
+            $this->sexo = $sexo;
+        }
     }
     
 
