54c809b8fc436095cdbb8a71d29bf26339786983
gabrielamenezes <gabimenezess10@gmail.com>
2021-01-29 20:26:46 -0300

    class animal + metodos abstratos

---
+++ animal_project/Animal.php
@@ -0,0 +1,13 @@
+<?php
+    abstract class Animal
+    {
+        private $peso;
+        private $idade;
+        private $membros;
+
+        abstract function locomover();
+        abstract function alimentar();
+        abstract function emitirSom();
+    }
+
+?>
