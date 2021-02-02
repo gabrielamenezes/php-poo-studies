213cbb358466441bdf2eb3afcaf5c480d4cb6a1b
gabrielamenezes <gabimenezess10@gmail.com>
2021-02-01 21:58:39 -0300

    add atr e métodos de gafanhoto

--- video_project/Gafanhoto.php
+++ video_project/Gafanhoto.php
@@ -2,7 +2,24 @@
     require_once("Pessoa.php");
     class Gafanhoto extends Pessoa
     {
+        private $login;
+        private $totAssistido;
         
+        private function viuMaisUm() {
+            $this->setTotAssistido($this->getTotAssistido() + 1);
+        }
+        public function getLogin() {
+            return $this->login;
+        }
+        public function setLogin($login) {
+            $this->login = $login;
+        }
+        public function getTotAssistido() {
+            return $this->totAssistido;
+        }
+        public function setTotAssistido($tot) {
+            $this->totAssistido = $tot;
+        }
     }
     
 
