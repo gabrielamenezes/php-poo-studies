3c5b3da7762999a91a6824a2c62e9198717b37f8
gabrielamenezes <gabimenezess10@gmail.com>
2021-02-02 10:34:16 -0300

    instanciando objetos

---
+++ video_project/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+    <?php
+        require_once("Video.php");
+        require_once("Gafanhoto.php");
+        $v[0] = new Video("Aula 1 de POO");
+        $v[1] = new Video("Aula 12 de PHP");
+        $v[2] = new Video("Aula 3 de GIT");
+        $v[2]->play();
+        print_r($v);
+        $g = new Gafanhoto();
+        $g->setNome("Gabriela");
+        $g->setSexo("F");
+        $g->setIdade(21);
+        $g->setExp(50);
+        //$g->ganhaExp();
+        print_r($g);
+    ?>
+    </pre>
+</body>
+</html>
