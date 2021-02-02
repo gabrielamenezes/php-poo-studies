729d243e940e9842c93408aee20f9083eaf1258f
gabrielamenezes <gabimenezess10@gmail.com>
2021-02-02 10:37:15 -0300

    add construct

--- video_project/Pessoa.php
+++ video_project/Pessoa.php
@@ -6,6 +6,14 @@
         protected $sexo;
         protected $experiencia;
 
+        public function __construct($nome, $idade, $sexo)
+        {
+            $this->nome = $nome;
+            $this->idade = $idade;
+            $this->sexo = $sexo;
+            $this->experiencia = 0;
+        }
+
         protected function ganhaExp() {
             $this->setExp($this->getExp() + 5);
         }
