be859430bbcfe897bdcf10516afc201d10fab725
gabrielamenezes <gabimenezess10@gmail.com>
2021-02-02 16:40:30 -0300

    alteração nos métodos de avaliação

--- video_project/Visualizacao.php
+++ video_project/Visualizacao.php
@@ -11,16 +11,30 @@
             //sempre que instanciar essa classe, informar qual o espectador e filme
             $this->espectador = $espec;
             $this->filme = $filme;
+            //quando a visualização começar, dar +1 view para o vídeo
+            $this->filme->setViews($this->filme->getViews() + 1);
+            $this->espectador->setTotAssistido($this->espectador->getTotAssistido() + 1);
         }
         //poliformismo de sobrecarga no php
-        public function avaliar($msg) {
-            echo "<p> COMENTÁRIO: " . $msg . "</p>";
+        public function avaliar() {
+            $this->filme->setAvaliacao(5);
         }
         public function avaliarN($nota) {
-            echo "<p> NOTA: " . $nota . "</p>";
+            $this->filme->setAvaliacao($nota);
         }
         public function avaliarP($perc) {
-            echo "<p> PERCENTUAL: " . $perc . "</p>";
+            $nota = 0;
+            if($perc < 20) {
+                $nota = 3;
+            } else if($perc <= 50) {
+                $nota = 5;
+            } else if($perc <= 80) {
+                $nota = 8;
+            } else {
+                $nota = 10;
+            }
+        
+            $this->filme->setAvaliacao($nota);
         }
 
         //métodos getters e setters
